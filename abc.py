f746e3f91e0a4bd051916de13c1b03d48823fb15
Tom <nguyen.thom2406@gmail.com>
2025-02-27 15:56:15 +0700

    add new file

---
+++ abc.py
@@ -0,0 +1,8 @@
+from sentence_transformers import SentenceTransformer
+
+# INPUT TEXT MUST BE ALREADY WORD-SEGMENTED!
+sentences = ["Cô ấy là một người vui_tính .", "Cô ấy cười nói suốt cả ngày ."]
+
+model = SentenceTransformer('bkai-foundation-models/vietnamese-bi-encoder')
+embeddings = model.encode(sentences)
+print(embeddings)
